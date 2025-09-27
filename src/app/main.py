ac43fc6ea7030ac34b05841e1b43233609c33831
lamine93 <mamalee393@gmail.com>
2025-09-27 23:57:09 +0200

    add query by item

--- src/app/main.py
+++ src/app/main.py
@@ -1,6 +1,12 @@
 import os
-from fastapi import FastAPI
+
 from azure.cosmos import CosmosClient, PartitionKey, exceptions
+from fastapi import FastAPI, HTTPException, Query
+from pydantic import BaseModel
+from typing import Any, Dict
+
+class ItemUpdate(BaseModel):
+    data: Dict[str, Any]
 
 ENDPOINT = os.environ["COSMOS_ENDPOINT"]
 KEY      = os.environ["COSMOS_KEY"]
@@ -26,3 +32,30 @@ def add_item(item: dict):
 @app.get("/items")
 def list_items():
     return list(container.read_all_items())
+
+@app.get("/items/{item_id}")
+def get_item(item_id: str, pk: str = Query("default", description="Partition key")):
+    try:
+        doc = container.read_item(item=item_id, partition_key=pk)
+        return doc
+    except exceptions.CosmosResourceNotFoundError:
+        raise HTTPException(status_code=404, detail=f"Item {item_id} not found in pk='pk'")
+    
+
+@app.put("/items/{item_id}")
+def update_item(item_id: str, payload: ItemUpdate, pk: str = Query("default")):
+    try:
+        doc = container.read_item(item=item_id, partition_key=pk)
+        doc.update(payload.data) # mets à jour les champs
+        saved = container.replace_item(item=doc, body=doc)
+        return saved
+    except exceptions.CosmosResourceNotFoundError:
+        raise HTTPException(status_code=404, detail=f"Item {item_id} not found in pk='{pk}'")
+
+@app.delete("/items/{item_id}")
+def delete_item(item_id: str, pk: str = Query("default")):
+    try:
+        container.delete_item(item=item_id, partition_key=pk)
+        return {"deleted": True, "id": item_id, "pk": pk}
+    except exceptions.CosmosResourceNotFoundError:
+        raise HTTPException(status_code=404, detail=f"Item {item_id} not found in pk='{pk}'")
